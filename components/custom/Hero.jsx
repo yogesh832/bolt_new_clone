28eed087e988b3731234f684ff4e00c58639b098
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-01-29 18:32:47 +0530

    add thsslkdsd

--- components/custom/Hero.jsx
+++ components/custom/Hero.jsx
@@ -4,7 +4,7 @@ import { MessagesContext } from "@/app/Context/MessagesContext";
 import { userDetailContext } from "@/app/Context/UserDetailContext";
 import Colors from "@/data/Colors";
 import Lookup from "@/data/Lookup";
-import { ArrowRight, Link } from "lucide-react";
+import { ArrowRight, Link, Loader2Icon } from "lucide-react";
 import SignInDialog from "./SignInDialog";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
@@ -17,8 +17,10 @@ const Hero = () => {
   const { userDetail, setUserDetail } = useContext(userDetailContext);
   const router = useRouter();
   const createWorkspace = useMutation(api.Workspace.createWorkspace);
+const [loading, setLoading]= useState(false)
 
   const onGenerate = async (input) => {
+    setLoading(true)
     if (!userDetail) {
       setOpenDialog(true);
       return;
@@ -34,6 +36,7 @@ const Hero = () => {
       message: [msg],
     });
     console.log(workspaceId);
+    setLoading(false)
     router.push("/workspace/" + workspaceId);
   };
 
@@ -77,6 +80,11 @@ const Hero = () => {
       </div>
 
       <SignInDialog openDialog={openDialog} closeDialog={(v) => setOpenDialog(v)} />
+      {loading &&   <div className=" flex p-10 bg-gray-900 opacity-80 absolute
+       top-0 rounded-lg w-full h-full items-center justify-center">
+        <Loader2Icon className='animate-spin h-10 w-10 text-white'/>
+        <h2 className='text-white'>Generating Your Files....</h2>
+      </div>}
     </div>
   );
 };
